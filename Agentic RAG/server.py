1d2ac54d8ce712820ace8c3cfa140ab30e91201e
Arjun <niteshkrjhag@gmail.com>
2026-05-04 23:02:47 +0530

    Setting Up server

--- Agentic RAG/server.py
+++ Agentic RAG/server.py
@@ -1,4 +1,9 @@
 from crewai import Crew, Agent, Task, LLM
 import litserve as ls
-from firecrawl import Firecrawl
 
+from fire_crawl import fc
+import vector_search
+
+class AgenticRAGAPI(ls.LitAPI):
+    def setup(self, device):
+        llm = LLM(model="ollama/qwen3")
