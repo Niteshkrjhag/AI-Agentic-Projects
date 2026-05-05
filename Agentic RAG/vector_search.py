7865f74f7153491a80c140437bc0920d23ab5728
Arjun <niteshkrjhag@gmail.com>
2026-05-05 22:43:03 +0530

    converted vector_search.py to be used as tool by agent

--- Agentic RAG/vector_search.py
+++ Agentic RAG/vector_search.py
@@ -3,41 +3,44 @@ import numpy as np
 import requests
 from typing import List, Dict, Any
 
-dimension = 768
 
-#In the context of vector databases and libraries like FAISS (Facebook AI Similarity Search), an index is a 
-#specialized data structure used to store and quickly search through large collections of high-dimensional vectors.
-index = faiss.IndexFlatL2(dimension)
-documents = []
+class VectorSearchTool:
+    def __init__(self):
 
+        self.dimension = 768
 
-def get_embedding(text):
-    response = requests.post(
-
-        "http://localhost:11434/api/embeddings",
-
-        json={"model": "nomic-embed-text", "prompt": text}
-
-    )
-    return response.json()["embedding"]
+        #In the context of vector databases and libraries like FAISS (Facebook AI Similarity Search), an index is a 
+        #specialized data structure used to store and quickly search through large collections of high-dimensional vectors.
+        self.index = faiss.IndexFlatL2(self.dimension)
+        self.documents = []
 
 
-def add_docs(texts):
-    global documents
-    embeddings = [get_embedding(text) for text in texts]
-    vectors  = np.array(embeddings).astype("float32")
-    index.add(vectors)
-    documents.extend(texts)
+    def get_embedding(self, text: str):
+        response = requests.post(
+
+            "http://localhost:11434/api/embeddings",
+
+            json={"model": "nomic-embed-text", "prompt": text}
+
+            )
+        return response.json()["embedding"]
 
 
-def search(query, k = 3):
-    query_vector = np.array([get_embedding(query)]).astype("float32")
-    distances, indexes = index.search(query_vector, k)
-    results = [(s, documents[i]) for s, i in zip(distances[0],indexes[0])]
-    return results # arranged in lower the distance better closer the alignment of document to the query
+    def add_docs(self, texts: List[str]):
+        global documents
+        embeddings = [self.get_embedding(text) for text in texts]
+        vectors  = np.array(embeddings).astype("float32")
+        self.index.add(vectors)
+        self.documents.extend(texts)
 
 
-print(len(get_embedding("test")))
-add_docs(["AI is artificial Intelligence", "Python is a programming language", "Delhi is the capital of India"])
-results = search("what is AI")
-print(results)
+    def run(self, query: str, k: int = 3):
+        query_vector = np.array([self.get_embedding(query)]).astype("float32")
+        distances, indexes = self.index.search(query_vector, k)
+        
+        results = [
+            {"score": float(dist), "document": self.documents[i]}
+            for dist, i in zip(distances[0], indexes[0])
+        ]
+        return results # arranged in lower the distance better closer the alignment of document to the query
+
